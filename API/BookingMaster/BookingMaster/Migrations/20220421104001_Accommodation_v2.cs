08a2ecc953d393936de44e380c56834e266c00aa
Kovács Andrea <79245705+kandi00@users.noreply.github.com>
2022-04-21 13:41:19 +0300

    migration added

---
+++ API/BookingMaster/BookingMaster/Migrations/20220421104001_Accommodation_v2.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookingMaster.Migrations
+{
+    public partial class Accommodation_v2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
